d7a4d8b9bebceb3e4ad529abd1e2bdd727035ce9
indra.x.stream@gmail.com <indra.x.stream@gmail.com>
2020-04-04 20:21:44 +0800

    Soal no.3 selesai

--- 3.py
+++ 3.py
@@ -1,12 +1,13 @@
-from itertools import islice 
-def divide(par1, par2):
-    length = len(par1)
+def divide(mylist, chunk_size):
     result = []
-    l = []
-    for i in range(0, length, par2):
-        l.append(par1[i])
-    print(l)
-    return True
+    for i in range(0, len(mylist), chunk_size):
+        result.append(mylist[i:i+chunk_size])
+
+    if len(result[-1]) == 1 and chunk_size != 1:
+        result[-2].append(*result[-1])
+        del result[-1]
+    return print(result)
+
 
 divide([1,2,3,4,5], 2)
 divide([1,2,3,4,5], 3)
